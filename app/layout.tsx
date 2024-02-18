0bfddf80254b30ad658ba925676d51c4c091824a
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-02-19 04:20:40 +0530

    started with gdsc rewrite

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Toaster } from 'react-hot-toast'
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
 
 export const metadata = {
     title: 'GDSC MIT WPU',
@@ -14,12 +16,14 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
     return (
         <html lang="en">
-            <body className={`text-text`}>
-                <main className="flex min-h-screen flex-col items-center justify-between bg-background p-24">
-                    {children}
-                </main>
-                <Toaster />
-            </body>
+        <body className={`text-text bg-background dark`}>
+        <Navbar />
+        <main className="flex min-h-screen flex-col items-center justify-between bg-background p-24 pt-32">
+            {children}
+        </main>
+        <Toaster />
+        <Footer />
+        </body>
         </html>
     )
 }
