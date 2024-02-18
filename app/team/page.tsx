d090840fefbc4238bf77c420452de024411da17e
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-02-19 04:59:40 +0530

    Team Page

---
+++ app/team/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TeamsList from '@/components/Teams/TeamsList'
+
+const TeamPage = () => {
+    return (
+        <>
+            <TeamsList title={'Tech Team'} />
+        </>
+    )
+}
+
+export default TeamPage
