caad12a9c0e58b0f4c58581c42da8f91dbd1c8f9
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-02-19 04:59:31 +0530

    Podcasts Page

---
+++ app/podcasts/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PodcastsList from '@/components/Podcasts/PodcastsList'
+
+const PodcastsPage = () => {
+    return (
+        <>
+            <PodcastsList title={'All Podcasts'} />
+        </>
+    )
+}
+
+export default PodcastsPage
---
+++ components/Podcasts/PodcastsList.tsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Skeleton } from '@/components/ui/skeleton'
+import Link from 'next/link'
+import { cn } from '@/lib/utils'
+import { buttonVariants } from '@/components/ui/button'
+
+
+interface PodcastsListProps {
+    title: string
+}
+
+const PodcastsList = ({ title }: PodcastsListProps) => {
+    return (
+        <div>
+            <h2 className={'text-3xl lg:text-4xl mb-10'}>{title}</h2>
+            <div className="events-grid grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
+                <Link href={'#'}>
+                    <Card>
+                        <CardHeader>
+                            <Skeleton className={'w-full aspect-square'} />
+                        </CardHeader>
+                        <CardContent>
+                            <CardTitle className={'text-lg'}>Podcast Title</CardTitle>
+                        </CardContent>
+                    </Card>
+                </Link>
+                <Link href={'#'}>
+                    <Card>
+                        <CardHeader>
+                            <Skeleton className={'w-full aspect-square'} />
+                        </CardHeader>
+                        <CardContent>
+                            <CardTitle className={'text-lg'}>Podcast Title</CardTitle>
+                        </CardContent>
+                    </Card>
+                </Link>
+                <Link href={'#'}>
+                    <Card>
+                        <CardHeader>
+                            <Skeleton className={'w-full aspect-square'} />
+                        </CardHeader>
+                        <CardContent>
+                            <CardTitle className={'text-lg'}>Podcast Title</CardTitle>
+                        </CardContent>
+                    </Card>
+                </Link>
+                <Link href={'#'}>
+                    <Card>
+                        <CardHeader>
+                            <Skeleton className={'w-full aspect-square'} />
+                        </CardHeader>
+                        <CardContent>
+                            <CardTitle className={'text-lg'}>Podcast Title</CardTitle>
+                        </CardContent>
+                    </Card>
+                </Link>
+                <Link href={'#'}>
+                    <Card>
+                        <CardHeader>
+                            <Skeleton className={'w-full aspect-square'} />
+                        </CardHeader>
+                        <CardContent>
+                            <CardTitle className={'text-lg'}>Podcast Title</CardTitle>
+                        </CardContent>
+                    </Card>
+                </Link>
+                <Link href={'#'}>
+                    <Card>
+                        <CardHeader>
+                            <Skeleton className={'w-full aspect-square'} />
+                        </CardHeader>
+                        <CardContent>
+                            <CardTitle className={'text-lg'}>Podcast Title</CardTitle>
+                        </CardContent>
+                    </Card>
+                </Link>
+                <Link href={'#'}>
+                    <Card>
+                        <CardHeader>
+                            <Skeleton className={'w-full aspect-square'} />
+                        </CardHeader>
+                        <CardContent>
+                            <CardTitle className={'text-lg'}>Podcast Title</CardTitle>
+                        </CardContent>
+                    </Card>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default PodcastsList
