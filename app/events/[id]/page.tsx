db7693eb8c0be7df7443b7998ab4ecad8b9a152c
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-02-19 16:45:37 +0530

    integrated individual events page

--- app/events/[id]/page.tsx
+++ app/events/[id]/page.tsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import Link from 'next/link'
-import { GitHubLogoIcon, InstagramLogoIcon, LinkedInLogoIcon } from '@radix-ui/react-icons'
-import { Facebook, Twitter } from 'react-feather'
-import { Button } from '@/components/ui/button'
+import { GitHubLogoIcon, InstagramLogoIcon, LinkedInLogoIcon, } from '@radix-ui/react-icons'
+import { Facebook, Navigation, Navigation2, Twitter } from 'react-feather'
+import { Button, buttonVariants } from '@/components/ui/button'
 import { Skeleton } from '@/components/ui/skeleton'
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { collection, doc, getDoc, getFirestore } from '@firebase/firestore'
 import { app } from '@/lib/firebase'
 import { redirect, RedirectType } from 'next/navigation'
+import Image from 'next/image'
+import { cn } from '@/lib/utils'
+
 interface EventPageProps {
     params: {
         id: string
     }
 }
-const EventPage = async ({params:{id}}:EventPageProps) => {
+
+const EventPage = async ({ params: { id } }: EventPageProps) => {
     const db = getFirestore(app)
     const eventsCollection = collection(db, 'events')
     const querySnapshot = await getDoc(doc(eventsCollection, id))
@@ -26,10 +30,9 @@ const EventPage = async ({params:{id}}:EventPageProps) => {
             <section
                 className={'w-full flex gap-20 flex-col lg:flex-row  p-8 lg:p-24 pt-32 lg:pt-32'}>
                 <div className="content w-full lg:w-1/2">
-                    <h1 className={'text-6xl'}>Event Title</h1>
+                    <h1 className={'text-6xl'}>{data.title}</h1>
                     <p className="description text-muted text-lg mt-4 max-w-3xl">
-                        At GDSC MIT-WPU, we nurture a culture of creativity and collaboration. We aim to explore endless
-                        possibilities of technology, with the support and guidance of a warm community.
+                        {data.multiLineDescription}
                     </p>
                     <div className="links flex gap-4 mt-8">
                         <Link href={'#'} className={''}>
@@ -47,25 +50,33 @@ const EventPage = async ({params:{id}}:EventPageProps) => {
                     </div>
                 </div>
                 <div className="image w-full lg:w-1/2">
-                    <Skeleton className={'mx-auto w-full aspect-video'} />
+                    <div className="mx-auto w-full aspect-video relative rounded-xl overflow-hidden">
+                        <Image src={data.bannerImageLinks[0]} alt={data.title} fill={true}
+                               style={{ objectFit: 'cover' }} />
+                        <Skeleton className={'mx-auto w-full aspect-video'} />
+                    </div>
                 </div>
             </section>
             <section className={'p-8 lg:p-24 lg:px-0'}>
                 <Carousel className="w-10/12 mx-auto">
                     <CarouselContent>
-                        {Array.from({ length: 5 }).map((_, index) => (
+                        {data.agenda.map((agenda, index) => (
                             <CarouselItem className={'md:basis-1/2 lg:basis-1/3'} key={index}>
-                                <div className="p-1">
-                                    <Card>
+                                <div className="p-1 h-full max-h-80">
+                                    <Card className={'h-full overflow-auto'}>
                                         <CardHeader>
                                             <CardTitle>6th February</CardTitle>
                                             <CardDescription>12:00 pm</CardDescription>
                                         </CardHeader>
                                         <CardContent>
                                             <ul className={'flex flex-col'}>
-                                                <li className={'py-2 border-b border-muted'}>Agenda 1</li>
-                                                <li className={'py-2 border-b border-muted'}>Agenda 1</li>
-                                                <li className={'py-2 border-b border-muted'}>Agenda 1</li>
+                                                {agenda.activities.map((activity, index) => {
+                                                    return (
+                                                        <li className={'py-2 border-b border-muted'} key={index}>
+                                                            {activity}
+                                                        </li>
+                                                    )
+                                                })}
                                                 <li className={'py-2 border-b border-muted'}>Agenda 1</li>
                                             </ul>
                                         </CardContent>
@@ -78,20 +89,23 @@ const EventPage = async ({params:{id}}:EventPageProps) => {
                     <CarouselNext />
                 </Carousel>
             </section>
-            <section className={'p-8 lg:p-24 bg-secondary'}>
+            {data.type === 'Upcoming' && <section className={'p-8 lg:p-24 bg-secondary'}>
                 <p className="text-muted text-xl lg:text-2xl mb-4 ">
                     16th April - 18th April
                 </p>
-                <h2 className={'text-4xl lg:text-5xl max-w-4xl'}>Vk Hall, Mit World Peace University, Kothrud, Pune</h2>
-            </section>
-            <section className={'p-8 lg:p-24 lg:px-0'}>
+                <h2 className={'text-4xl lg:text-5xl max-w-4xl'}>{data.venue}</h2>
+                <Link href={data.googleMapLink} className={cn(buttonVariants({ variant: 'outline' }),"flex items-center gap-5 w-min mt-10")}><Navigation2
+                    height={20} width={20} /> Locate</Link>
+            </section>}
+            {data.type === 'Past' && <section className={'p-8 lg:p-24 lg:px-0'}>
                 <h2 className={'mx-8 lg:mx-24 text-5xl mb-10'}>Gallery</h2>
                 <Carousel className="w-10/12 mx-auto">
                     <CarouselContent>
-                        {Array.from({ length: 5 }).map((_, index) => (
+                        {data.flashbackImageLinks.map((image, index) => (
                             <CarouselItem className={'md:basis-1/2 lg:basis-1/3'} key={index}>
-                                <div className="p-1">
-                                    <Skeleton className={'w-full aspect-square'} />
+                                <div className="w-full aspect-square relative rounded overflow-hidden">
+                                    <Image src={image} alt={''} fill={true} />
+                                    <Skeleton className={'w-full h-full absolute top-0 left-0'} />
                                 </div>
                             </CarouselItem>
                         ))}
@@ -99,7 +113,7 @@ const EventPage = async ({params:{id}}:EventPageProps) => {
                     <CarouselPrevious />
                     <CarouselNext />
                 </Carousel>
-            </section>
+            </section>}
         </>
     )
 }
--- types/event.d.ts
+++ types/event.d.ts
@@ -4,9 +4,16 @@ interface IEventDoc {
     googleMapLink: string;
     id: string;
     title: string;
-    agenda: Array<Object>;
+    agenda: IEventAgenda[]
     singleLineDescription: string;
     type: string;
     flashbackImageLinks: Array<string>;
     bannerImageLinks: Array<string>;
+}
+
+interface IEventAgenda {
+    activities: string[]
+    data: string
+    starts_at: string
+    ends_at: string
 }
