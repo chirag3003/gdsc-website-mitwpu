490f4cf849daf5ba6e1bd4b6e9544cac0330bcb8
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-02-19 12:59:13 +0530

    updates

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -3,19 +3,23 @@ import { Skeleton } from '@/components/ui/skeleton'
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
 import { Card, CardContent } from '@/components/ui/card'
 import TeamsList from '@/components/Teams/TeamsList'
-import { Button } from '@/components/ui/button'
+import { Button, buttonVariants } from '@/components/ui/button'
 import { GitHubLogoIcon } from '@radix-ui/react-icons'
+import Link from 'next/link'
+import { cn } from '@/lib/utils'
 
 const ProjectPage = () => {
     return (
         <>
             <section className={'flex flex-col lg:flex-row gap-20 mb-20'}>
                 <div className="content w-full lg:w-1/2">
-                    <h1 className={'text-2xl lg:text-4xl'}>How you can train an AI to convert your design mockups into HTML and
+                    <h1 className={'text-2xl lg:text-4xl'}>How you can train an AI to convert your design mockups into
+                        HTML and
                         CSS8</h1>
                     <p className={'lg:text-xl text-muted mt-6'}>Lorem Ipsum Lorem Ipsum Lorem Ipsum </p>
                     <div className="buttons mt-12 flex gap-5">
-                        <Button className={"flex gap-5"}><GitHubLogoIcon height={20} width={20} /> Github </Button>
+                        <Link className={cn(buttonVariants({ variant: 'secondary', size: "lg" }), 'flex gap-5')}
+                              href={'#'}><GitHubLogoIcon height={20} width={20} /> Github </Link>
                     </div>
                 </div>
                 <div className="banner w-full lg:w-1/2">
@@ -23,7 +27,7 @@ const ProjectPage = () => {
                 </div>
             </section>
             <section className="images mb-20">
-                <h2 className={"text-4xl mb-10"}>Project Images</h2>
+                <h2 className={'text-4xl mb-10'}>Project Images</h2>
                 <Carousel className="w-10/12 mx-auto">
                     <CarouselContent>
                         {Array.from({ length: 5 }).map((_, index) => (
