f22deeae99cae9459e2166c24e50c1e67c67efa3
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-02-19 05:38:04 +0530

    team list margin

--- components/Teams/TeamsList.tsx
+++ components/Teams/TeamsList.tsx
@@ -14,7 +14,7 @@ interface TeamsListProps {
 
 const TeamsList = ({title}:TeamsListProps) => {
     return (
-        <div>
+        <div className={"mb-20"}>
             <h2 className={'text-3xl lg:text-4xl mb-10'}>{title}</h2>
             <div className="events-grid grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
                 <Card>
