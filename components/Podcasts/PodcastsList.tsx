dbade1a8c824f66bb83ab98c6d47d39885020288
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-02-20 17:01:29 +0530

    updated podcasts ui

--- components/Podcasts/PodcastsList.tsx
+++ components/Podcasts/PodcastsList.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
 import { Skeleton } from '@/components/ui/skeleton'
 import Link from 'next/link'
 import Image from 'next/image'
@@ -14,24 +21,42 @@ const PodcastsList = ({ title, podcasts }: PodcastsListProps) => {
         <div className={'mb-20'}>
             <h2 className={'text-3xl lg:text-4xl mb-10'}>{title}</h2>
             <div className="events-grid grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
-                {podcasts.map(({ title, spotifyLink, bannerImgLink }, index) => {
-                    return (
-                        <Link key={index} href={spotifyLink} target={'_blank'} referrerPolicy={'no-referrer'}>
-                            <Card>
-                                <CardHeader>
-                                    <div className="w-full relative aspect-square rounded-xl overflow-hidden">
-                                        <Skeleton className={'w-full aspect-square'} />
-                                        <Image src={bannerImgLink} alt={title} fill={true} />
-                                    </div>
-                                </CardHeader>
-                                <CardContent>
-                                    <CardTitle className={'text-lg'}>{title}</CardTitle>
-                                </CardContent>
-                            </Card>
-                        </Link>
-
-                    )
-                })}
+                {podcasts.map(
+                    ({ title, spotifyLink, bannerImgLink }, index) => {
+                        return (
+                            <Link
+                                key={index}
+                                href={spotifyLink}
+                                target={'_blank'}
+                                referrerPolicy={'no-referrer'}
+                            >
+                                <Card>
+                                    <CardHeader>
+                                        <div className="w-full relative aspect-square rounded-xl overflow-hidden">
+                                            <Skeleton
+                                                className={
+                                                    'w-full aspect-square'
+                                                }
+                                            />
+                                            <Image
+                                                src={bannerImgLink}
+                                                alt={title}
+                                                fill={true}
+                                            />
+                                        </div>
+                                    </CardHeader>
+                                    <CardContent>
+                                        <CardTitle>
+                                            <p className="w-full text-ellipsis overflow-hidden whitespace-nowrap">
+                                                {title} afsdf asdaf sdf as d
+                                            </p>
+                                        </CardTitle>
+                                    </CardContent>
+                                </Card>
+                            </Link>
+                        )
+                    }
+                )}
             </div>
         </div>
     )
