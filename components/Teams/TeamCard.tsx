b37bcb21f54f62d77a483fecaddb162ee9126c45
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-02-23 14:04:37 +0530

    updated team card

--- components/Teams/TeamCard.tsx
+++ components/Teams/TeamCard.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
 import { Skeleton } from '@/components/ui/skeleton'
 import Link from 'next/link'
 import { cn } from '@/lib/utils'
 import { buttonVariants } from '@/components/ui/button'
 import { Mail } from 'react-feather'
-import { GitHubLogoIcon, InstagramLogoIcon, LinkedInLogoIcon } from '@radix-ui/react-icons'
+import {
+    GitHubLogoIcon,
+    InstagramLogoIcon,
+    LinkedInLogoIcon,
+} from '@radix-ui/react-icons'
 import Image from 'next/image'
 
 export interface TeamCardProps {
@@ -18,38 +29,87 @@ export interface TeamCardProps {
     instagram?: string
 }
 
-const TeamCard = ({ name, role, instagram, email, github, imageUrl, linkedin }: TeamCardProps) => {
+const TeamCard = ({
+    name,
+    role,
+    instagram,
+    email,
+    github,
+    imageUrl,
+    linkedin,
+}: TeamCardProps) => {
     return (
         <Card>
             <CardHeader>
                 <div className="relative w-full aspect-square rounded-xl overflow-hidden">
-                    <Image src={imageUrl} alt={name} fill={true} />
-                    <Skeleton className={'w-full h-full absolute top-0 left-0 right-0'} />
+                    <Image
+                        src={imageUrl}
+                        alt={name}
+                        fill={true}
+                        style={{ objectFit: 'cover' }}
+                    />
+                    <Skeleton
+                        className={
+                            'w-full h-full absolute top-0 left-0 right-0'
+                        }
+                    />
                 </div>
             </CardHeader>
             <CardContent>
                 <CardTitle className={'mb-1'}>{name}</CardTitle>
-                <CardDescription className={'text-muted '}>{role}</CardDescription>
+                <CardDescription className={'text-muted '}>
+                    {role}
+                </CardDescription>
             </CardContent>
             <CardFooter className={'flex items-center gap-3 justify-end mt-4'}>
-                <Link href={email} className={cn(buttonVariants({ size: 'icon', variant: 'ghost' }), '')}><Mail
-                    width={24}
-                    height={24} /></Link>
-                {linkedin && <Link href={linkedin}
-                                   className={cn(buttonVariants({
-                                       size: 'icon',
-                                       variant: 'ghost',
-                                   }), '')}><LinkedInLogoIcon
-                    width={24} height={24} /></Link>}
-                {github && <Link href={github}
-                                 className={cn(buttonVariants({ size: 'icon', variant: 'ghost' }), '')}><GitHubLogoIcon
-                    width={24} height={24} /></Link>}
-                {instagram && <Link href={instagram}
-                                    className={cn(buttonVariants({
-                                        size: 'icon',
-                                        variant: 'ghost',
-                                    }), '')}><InstagramLogoIcon
-                    width={24} height={24} /></Link>}
+                <Link
+                    href={email}
+                    className={cn(
+                        buttonVariants({ size: 'icon', variant: 'ghost' }),
+                        ''
+                    )}
+                >
+                    <Mail width={24} height={24} />
+                </Link>
+                {linkedin && (
+                    <Link
+                        href={linkedin}
+                        className={cn(
+                            buttonVariants({
+                                size: 'icon',
+                                variant: 'ghost',
+                            }),
+                            ''
+                        )}
+                    >
+                        <LinkedInLogoIcon width={24} height={24} />
+                    </Link>
+                )}
+                {github && (
+                    <Link
+                        href={github}
+                        className={cn(
+                            buttonVariants({ size: 'icon', variant: 'ghost' }),
+                            ''
+                        )}
+                    >
+                        <GitHubLogoIcon width={24} height={24} />
+                    </Link>
+                )}
+                {instagram && (
+                    <Link
+                        href={instagram}
+                        className={cn(
+                            buttonVariants({
+                                size: 'icon',
+                                variant: 'ghost',
+                            }),
+                            ''
+                        )}
+                    >
+                        <InstagramLogoIcon width={24} height={24} />
+                    </Link>
+                )}
             </CardFooter>
         </Card>
     )
