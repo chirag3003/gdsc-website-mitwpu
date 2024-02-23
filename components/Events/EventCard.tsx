26429e819434c0c5402178ca8bada1aab00f20b5
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-02-23 14:26:11 +0530

    updated event list card

--- components/Events/EventCard.tsx
+++ components/Events/EventCard.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
 import { Skeleton } from '@/components/ui/skeleton'
 import Link from 'next/link'
 import { cn } from '@/lib/utils'
@@ -18,16 +25,32 @@ const EventCard = ({ title, description, id, imageUrl }: EventCardProps) => {
         <Card>
             <CardHeader>
                 <div className="w-full aspect-video relative rounded-xl overflow-hidden">
-                    <Skeleton className={'w-full h-full absolute top-0 left-0 right-0'} />
-                    <Image src={imageUrl} alt={title} fill={true} />
+                    <Skeleton
+                        className={
+                            'w-full h-full absolute top-0 left-0 right-0'
+                        }
+                    />
+                    <Image
+                        src={imageUrl}
+                        alt={title}
+                        fill={true}
+                        style={{ objectFit: 'cover' }}
+                    />
                 </div>
             </CardHeader>
             <CardContent>
                 <CardTitle className={'mb-1 text-lg'}>{title}</CardTitle>
-                <CardDescription className={'text-muted'}>{description}</CardDescription>
+                <CardDescription className={'text-muted'}>
+                    {description}
+                </CardDescription>
             </CardContent>
             <CardFooter className={'flex items-center gap-3 justify-end mt-4'}>
-                <Link href={`/events/${id}`} className={cn(buttonVariants(), '')}>Details</Link>
+                <Link
+                    href={`/events/${id}`}
+                    className={cn(buttonVariants(), '')}
+                >
+                    Details
+                </Link>
             </CardFooter>
         </Card>
     )
