f3d54b22f59bc4b8704f01271a84b04ff1933bbd
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-02-26 00:22:26 +0530

    updated join us page

--- app/join-us/page.tsx
+++ app/join-us/page.tsx
@@ -1,5 +1,4 @@
 import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import {
     Select,
@@ -8,10 +7,10 @@ import {
     SelectTrigger,
     SelectValue,
 } from '@/components/ui/select'
-import { Textarea } from '@/components/ui/textarea'
 import React from 'react'
 
 function JoinUsPage() {
+    const isRecruiting = false
     return (
         <section className="p-8 lg:p-24 pt-32 lg:pt-32 grid lg:grid-cols-2 gap-20">
             <div className="text-content">
@@ -24,23 +23,28 @@ function JoinUsPage() {
                     building, and creating. Get involved and make a difference!
                     We look forward to welcoming you to the GDSC MITWPU family!
                 </p>
-                <div className="mt-10 py-2 px-4 bg-accent/50 rounded">Sorry, We are not recruiting right now</div>
+                <div className="mt-10 py-2 px-4 bg-accent/50 rounded">
+                    Sorry, We are not recruiting right now
+                </div>
             </div>
             <form className="max-w-lg w-full mx-auto flex flex-col gap-4">
                 <div className="grid w-full items-center gap-1.5">
                     <Label htmlFor="role">Preferred Role</Label>
                     <Select>
-                        <SelectTrigger id="role" className="w-full">
+                        <SelectTrigger disabled={!isRecruiting} id="role" className="w-full">
                             <SelectValue placeholder="Preferred Role" />
                         </SelectTrigger>
                         <SelectContent>
-                            <SelectItem value="light">Light</SelectItem>
-                            <SelectItem value="dark">Dark</SelectItem>
-                            <SelectItem value="system">System</SelectItem>
+                            <SelectItem value="tech">Tech</SelectItem>
+                            <SelectItem value="design">Design</SelectItem>
+                            <SelectItem value="pr">Pr & Sponsorship</SelectItem>
+                            <SelectItem value="management">
+                                Management
+                            </SelectItem>
                         </SelectContent>
                     </Select>
                 </div>
-                <Button className="text-lg w-full">Submit</Button>
+                <Button disabled={!isRecruiting} className="text-lg w-full">Submit</Button>
             </form>
         </section>
     )
