c8b8ae6e1a65f9492c535620f23d0af60cd9c9e9
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-02-26 00:42:31 +0530

    footer section socials

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,48 +1,26 @@
 import React from 'react'
-import { LinkedInLogoIcon, GitHubLogoIcon, InstagramLogoIcon, TwitterLogoIcon } from '@radix-ui/react-icons'
-import { Mail } from 'react-feather'
+import socials from '../configs/socials'
 
-const navigation = {
-    social: [
-        {
-            name:"Mail",
-            href: "#",
-            icon: Mail
-        },
-        {
-            name: 'Instagram',
-            href: '#',
-            icon: InstagramLogoIcon,
-        },
-        {
-            name: 'X',
-            href: '#',
-            icon: TwitterLogoIcon,
-        },
-        {
-            name: 'GitHub',
-            href: '#',
-            icon: GitHubLogoIcon,
-        },
-        {
-            name: 'Linkedin',
-            href: '#',
-            icon: LinkedInLogoIcon,
-        },
-    ],
-}
 const Footer = () => {
     return (
         <footer className="mt-8 border-t border-border/10 p-8 flex flex-col gap-9 md:flex-row md:items-center md:justify-between lg:px-24">
-
             <div className="logo">
-                <img className="h-14 w-auto" src={'/gdsc-logo.svg'}  alt={""}/>
+                <img className="h-14 w-auto" src={'/gdsc-logo.svg'} alt={''} />
             </div>
             <div className="flex space-x-6">
-                {navigation.social.map((item) => (
-                    <a key={item.name} href={item.href} className="text-foreground md:text-foreground/60 hover:text-foreground">
+                {socials.map((item, index) => (
+                    <a
+                        key={index}
+                        href={item.url}
+                        className="text-foreground md:text-foreground/60 hover:text-foreground"
+                    >
                         <span className="sr-only">{item.name}</span>
-                        <item.icon className="h-6 w-6" aria-hidden="true" height={20} width={20}/>
+                        <item.icon
+                            className="h-6 w-6"
+                            aria-hidden="true"
+                            height={20}
+                            width={20}
+                        />
                     </a>
                 ))}
             </div>
