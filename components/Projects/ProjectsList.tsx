837f942bbd04005f73f5d97a5386e5811f18f336
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-02-29 13:51:48 +0530

    updated projects card

--- components/Projects/ProjectsList.tsx
+++ components/Projects/ProjectsList.tsx
@@ -1,45 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
 
-const posts = [
-
-
-    {
-        id: 1,
-        title: 'Boost your conversion rate',
-        href: '/projects/id',
-        description:
-            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
-        imageUrl:
-            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
-        date: 'Mar 16, 2020',
-        datetime: '2020-03-16',
-    },
-    {
-        id: 1,
-        title: 'Boost your conversion rate',
-        href: '/projects/id',
-        description:
-            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
-        imageUrl:
-            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
-        date: 'Mar 16, 2020',
-        datetime: '2020-03-16',
-    },
-    {
-        id: 1,
-        title: 'Boost your conversion rate',
-        href: '/projects/id',
-        description:
-            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
-        imageUrl:
-            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
-        date: 'Mar 16, 2020',
-        datetime: '2020-03-16',
-    },
-    // More posts...
-]
-
 interface ProjectsListProps {
     projects: IProjectDoc[]
 }
@@ -60,11 +21,11 @@ const ProjectsList = ({ projects }: ProjectsListProps) => {
                             <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
                         </div>
                         <div>
-                            <div className="flex items-center gap-x-4 text-xs">
+                            {/* <div className="flex items-center gap-x-4 text-xs">
                                 <time dateTime={'2020-03-16'} className="text-muted">
                                     {'Mar 16, 2020'}
                                 </time>
-                            </div>
+                            </div> */}
                             <div className="relative max-w-xl">
                                 <h3 className="mt-3 text-lg font-semibold leading-6">
                                     <p>
