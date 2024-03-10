a5b466260b1a1b717c8038e276e4425c6e2c342a
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-03-10 13:31:37 +0530

    updated receiver mail

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -6,42 +6,42 @@ import { ZodError } from 'zod'
 import { ContactEmailHtml } from '@/components/Emails/contact'
 
 export async function POST(req: Request) {
-    try {
-        const body = contactInputValidator.parse(await req.json())
-        const name = `${body.firstName} ${body.lastName}`
-        await new Promise((resolve, reject) => {
-            transporter.sendMail(
-                {
-                    from: process.env.GMAIL_USERNAME,
-                    to: 'me@chirag.codes',
-                    subject: `You have received a message from ${name}`,
-                    html: render(
-                        ContactEmailHtml({
-                            name: name,
-                            email: body.email,
-                            message: body.message,
-                            phoneNo: body.phoneNo,
-                        })
-                    ),
-                },
-                function (err, info) {
-                    if (err) {
-                        reject(err)
-                    } else {
-                        resolve({})
-                    }
-                }
-            )
-        })
-        return new Response('ok')
-    } catch (err) {
-        if (err instanceof ZodError) {
-            return new Response(ReasonPhrases.BAD_REQUEST, {
-                status: StatusCodes.BAD_REQUEST,
+  try {
+    const body = contactInputValidator.parse(await req.json())
+    const name = `${body.firstName} ${body.lastName}`
+    await new Promise((resolve, reject) => {
+      transporter.sendMail(
+        {
+          from: process.env.GMAIL_USERNAME,
+          to: 'gdscmitofficial@mitwpu.edu.in',
+          subject: `You have received a message from ${name}`,
+          html: render(
+            ContactEmailHtml({
+              name: name,
+              email: body.email,
+              message: body.message,
+              phoneNo: body.phoneNo,
             })
+          ),
+        },
+        function(err, info) {
+          if (err) {
+            reject(err)
+          } else {
+            resolve({})
+          }
         }
-        return new Response(ReasonPhrases.INTERNAL_SERVER_ERROR, {
-            status: StatusCodes.INTERNAL_SERVER_ERROR,
-        })
+      )
+    })
+    return new Response('ok')
+  } catch (err) {
+    if (err instanceof ZodError) {
+      return new Response(ReasonPhrases.BAD_REQUEST, {
+        status: StatusCodes.BAD_REQUEST,
+      })
     }
+    return new Response(ReasonPhrases.INTERNAL_SERVER_ERROR, {
+      status: StatusCodes.INTERNAL_SERVER_ERROR,
+    })
+  }
 }
